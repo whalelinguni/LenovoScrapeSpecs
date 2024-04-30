4051707efeeccd01bb0d106113cbfb1d496507c4
whalelinguni <166869257+whalelinguni@users.noreply.github.com>
2024-04-30 13:24:58 -0500

    Update Lenovo-Scrape-HWSpecs.py

--- Lenovo-Scrape-HWSpecs.py
+++ Lenovo-Scrape-HWSpecs.py
@@ -1,16 +1,27 @@
-#working. warrnaty html save is trash. screen shot function is also trash.
+# working. warrnaty html save is trash. screen shot function is also trash.
 # python script.py <serial> [--mobile option] [--headless option] --wait <Num Seconds>
 
 import sys
 import time
+import os
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+clear_screen()
+
 # Check if the serial number is provided as a command-line argument
 if len(sys.argv) < 2:
-    print("Usage: python script_name.py <serial_number> [--headless] [--mobile] [--wait <seconds>]")
+    print(
+        "Usage: python script_name.py <serial_number> [--headless] [--mobile] [--wait <seconds>]"
+    )
     sys.exit(1)
 
 # Extract serial number from command-line arguments
@@ -24,6 +35,7 @@ print("Headless mode:", headless)
 mobile = "--mobile" in sys.argv
 print("Mobile mode:", mobile)
 
+
 # Extract wait time from command-line arguments
 wait_time = 5  # Default wait time in seconds
 if "--wait" in sys.argv:
@@ -36,18 +48,19 @@ if "--wait" in sys.argv:
 
 # Configure Firefox WebDriver to simulate a mobile device
 if mobile:
-    mobile_emulation = {
-        "deviceName": "iPhone X"
-    }
+    mobile_emulation = {"deviceName": "iPhone X"}
     options = webdriver.FirefoxOptions()
-    options.add_argument('--window-size=375,812')  # Set viewport size to match iPhone X
+    options.add_argument("--window-size=375,812")  # Set viewport size to match iPhone X
 else:
     options = webdriver.FirefoxOptions()
 
 if headless:
-    options.add_argument('--headless')  # Run browser in headless mode
+    options.add_argument("--headless")  # Run browser in headless mode
 
+print("Starting Verbose.")
+print("")
 # Initialize Firefox WebDriver with GeckoDriver
+print("Initializing selenium driver.")
 driver = webdriver.Firefox(options=options)
 
 try:
@@ -79,47 +92,45 @@ try:
     # Wait for the warranty information to load
     print(f"Waiting {wait_time} seconds for the warranty information to load...")
     time.sleep(wait_time)
-    
+
     # Get the HTML content of the page
     print("Getting the HTML content of the page...")
     html_content = driver.page_source
 
     ### I didn't really care to save this so it was not implimented. Just used the warranty page as a start point. No direct HW Specs page.
     # Save the HTML content to a file
-    #file_name = "warranty_info_mobile.html" if mobile else "warranty_info.html"
-    #if headless:
+    # file_name = "warranty_info_mobile.html" if mobile else "warranty_info.html"
+    # if headless:
     #    file_name = "headless_" + file_name
-    #with open(file_name, "w", encoding="utf-8") as f:
+    # with open(file_name, "w", encoding="utf-8") as f:
     #    f.write(html_content)
 
-    #print("Warranty information saved to", file_name)
+    # print("Warranty information saved to", file_name)
 
-    
     # Get the current URL of the browser
     current_url = driver.current_url
     print("Current URL:", current_url)
 
     # Remove '/warranty' from the end of the URL if it exists
-    if current_url.endswith('/warranty'):
-        current_url = current_url[:-len('/warranty')]
+    if current_url.endswith("/warranty"):
+        current_url = current_url[: -len("/warranty")]
         print("Machine URL:", current_url)
 
     # Navigate to a new URL
     print("Navigating to the machine URL...")
     driver.get(current_url)
     print("Waiting page to load...")
-    
+
     # Wait for the information to load  <---- this seems to have issues, which is why it just waits in seconds instead.
-    #print(f"Waiting for page to load...")
-    #page_info = WebDriverWait(driver, 10).until(
+    # print(f"Waiting for page to load...")
+    # page_info = WebDriverWait(driver, 10).until(
     #    EC.visibility_of_element_located((By.CLASS_NAME, "new-home-title"))
-    #)
+    # )
 
     # Wait for the information to load more
     print(f"Waiting {wait_time} seconds more for information to load...")
     time.sleep(wait_time)
-    
-    
+
     # Check if cookies popup is open and close
     try:
         close_button = driver.find_element(By.CLASS_NAME, "close-btn")
@@ -128,41 +139,51 @@ try:
         print("Closed the window.")
     except NoSuchElementException:
         print("Close button not found.")
-    
+
     # Scroll down by 500 pixels
     driver.execute_script("window.scrollBy(0, 300);")
 
     # Find the specs button using its XPath
     print("Waiting for the specs button...")
     submit_button = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/section[2]/div[2]/div[2]/div/div[2]/div[2]/section/div/div/div[4]/div[3]/div[2]/div/span"))
+        EC.visibility_of_element_located(
+            (
+                By.XPATH,
+                "/html/body/div[2]/section[2]/div[2]/div[2]/div/div[2]/div[2]/section/div/div/div[4]/div[3]/div[2]/div/span",
+            )
+        )
     )
 
     # Click the button
     print("Clicking on the specs button...")
     submit_button.click()
-    
+
     # Find the element containing the specs information
     print("Waiting for the specs information to load...")
     specs_element = WebDriverWait(driver, 10).until(
-    #EC.visibility_of_element_located((By.CLASS_NAME, "spec-info-title"))
-    EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/section[2]/div[2]/div[2]/div/div[2]/div[2]/section/div/div/div[4]/div[3]/div[2]/div/div"))
+        # EC.visibility_of_element_located((By.CLASS_NAME, "spec-info-title"))
+        EC.visibility_of_element_located(
+            (
+                By.XPATH,
+                "/html/body/div[2]/section[2]/div[2]/div[2]/div/div[2]/div[2]/section/div/div/div[4]/div[3]/div[2]/div/div",
+            )
         )
-        
-##screenshot <-- not as good as I thought it would be. Visually gets obstucted when screen shotting.
+    )
+
+    ##screenshot <-- not as good as I thought it would be. Visually gets obstucted when screen shotting.
     # Scroll the element into view if necessary (optional)
-#    print("Scrolling the element into view...")
-#    driver.execute_script("arguments[0].scrollIntoView();", specs_element)
+    #    print("Scrolling the element into view...")
+    #    driver.execute_script("arguments[0].scrollIntoView();", specs_element)
 
     # Take a screenshot of the specs element
-#    print("Taking a screenshot of the specs element...")
-#    specs_screenshot = specs_element.screenshot_as_png
+    #    print("Taking a screenshot of the specs element...")
+    #    specs_screenshot = specs_element.screenshot_as_png
 
     # Write the screenshot data to a file
-#    with open("specs_screenshot.png", "wb") as f:
-#        f.write(specs_screenshot)
+    #    with open("specs_screenshot.png", "wb") as f:
+    #        f.write(specs_screenshot)
 
-##save html        
+    ##save html
     # Find the element containing the specific class
     specific_element = driver.find_element(By.CLASS_NAME, "new-machinfo-desc")
 
@@ -186,6 +207,7 @@ try:
 
 except Exception as e:
     import traceback
+
     traceback.print_exc()
     print("An error occurred:", e)
 
@@ -193,3 +215,63 @@ finally:
     # Close the browser
     print("Closing the browser...")
     driver.quit()
+
+# Formatting html
+
+print("Parsing HTML file")
+
+# Load HTML file content
+with open(file_name, "r") as f:
+    html_content = f.read()
+
+# Parse HTML
+soup = BeautifulSoup(html_content, "html.parser")
+
+print("Extracting information.")
+# Extract title
+title = soup.find("h3", attrs={"t": "spec info|Spec Info"}).text
+
+# Extract information
+info = soup.find(
+    "span",
+    attrs={
+        "t": "machine info explain|The information contained on this website is for reference only, and its exclusive purpose is to facilitate the service delivery of the factory warranty services for Lenovo products. This information may change at any time without notice."
+    },
+).text
+
+# Extract configurations
+configs = []
+config_elements = soup.find_all(class_="desc-config-name")
+for element in config_elements:
+    name = element.text.strip()
+    detail = element.find_next(class_="desc-config-detail").text.strip()
+    configs.append((name, detail))
+
+print("Saving formatted information to text file")
+# Output formatted information
+output_file = os.path.splitext(file_name)[0] + ".txt"
+with open(output_file, "w") as f:
+    f.write("#####--- " + serial_number + " Hardware Configuration ---#####\n")
+    f.write("")
+    # f.write("Title: {}\n".format(title))
+    # f.write("Information: {}\n".format(info))
+    f.write("Configuration:\n")
+    for name, detail in configs:
+        f.write("  {}: {}\n".format(name, detail))
+
+print("Formatted information saved to", output_file)
+
+
+print("")
+print("")
+print("#####---", serial_number, "Hardware Configuration ---#####")
+print("")
+# Print to console
+# print("Title:", title)
+# print("Information:", info)
+print("Configuration:")
+for name, detail in configs:
+    print("  {}: {}".format(name, detail))
+
+print("")
+print("")
